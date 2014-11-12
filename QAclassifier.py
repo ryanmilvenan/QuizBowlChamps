b7b7708d010ef14d963843f31feec43e26fbf7bc
ReedAnders <anderson.reed@gmail.com>
2014-11-12 13:30:14 -0700

    Accuracy on dev: 0.716605

--- QAclassifier.py
+++ QAclassifier.py
@@ -3,26 +3,41 @@ from csv import DictReader, DictWriter
 
 import nltk
 from nltk.corpus import wordnet as wn
+from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
+
+def word_filter(word):
+    """
+    Simple stemmer
+    """
+    # Morphy returns the base form of a word, ie, dogs -> dog 
+    # unknown 'stem' returns word.lower()
+    stem = wn.morphy(word)
+    stop = stopwords.words('english')
+    if stem:
+        if stem.lower() not in stop:
+            return stem.lower()
+
 
 class FeatureExtractor:
 	def __init__(self):
 
 		# Single dict for feature vector
-		self._d = defaultdict(float)
+		# self._d = defaultdict(float)
+            None
 
-	def score_dict(self, vals):
-		d = self._d
+	def guess_dict(self, vals):
+		d = defaultdict(float)
 		for jj in vals.split(", "):
 			key, val = jj.split(":")
 			d[key.strip()] = float(val)
 		return d
 
 	def text_features(self, sentence):
-		d = self._d
+		d = defaultdict(float)
 		words = sentence.lower().split()
 		for ww in words:
-			d[ww] += 1
+			d[word_filter(ww)] += 1
 		return d
 
 
@@ -41,48 +56,114 @@ if __name__ == "__main__":
     train = DictReader(open("train.csv", 'r'))
     
     # Split off dev section
-    dev_train = []
-    dev_test = []
-    full_train = []
+    pos_train = []
+    oth_train = []
+
+    pos_test = []
+    oth_test = []
+    
+    # pos_full = []
+    # oth_full = []
 
     # Train 
     for ii in train:
         if args.subsample < 1.0 and int(ii['Question ID']) % 100 > 100 * args.subsample:
             continue
+
+        # The following calls return one <type 'collections.defaultdict'>
         feat = fe.text_features(ii['Question Text'])
-        # print ii['Question Text']
+        Q_guess = fe.guess_dict(ii['QANTA Scores'])
+        W_guess = fe.guess_dict(ii['IR_Wiki Scores'])
+        
+        # if ii['Answer'] == 'carthage' and ii['Sentence Position'] == '3':
+        #     print "A \n", ii['Answer']
+        #     print "Q \n", Q_guess, max(Q_guess, key=Q_guess.get)
+        #     print "W \n", W_guess, max(W_guess, key=W_guess.get)
+        #     print "T \n", feat
+        # print type(Q_guess)
+
         if int(ii['Question ID']) % 5 == 0:
-            # Appends feature stem and category key
-            dev_test.append((feat, ii['Answer']))
+            
+            # Append pos and oth for W_guess
+            if max(W_guess, key=W_guess.get) == ii['Answer']:
+                feat[ii['Answer']] = True
+                pos_test.append(feat)
+            else:
+                feat[ii['Answer']] = False
+                oth_test.append(feat)
+
+            # Append pos and oth for Q_guess
+            if max(Q_guess, key=Q_guess.get) == ii['Answer']:
+                feat[ii['Answer']] = True
+                pos_test.append(feat)
+            else:
+                feat[ii['Answer']] = False
+                oth_test.append(feat)
+        
         else:
-        	dev_train.append((feat, ii['Answer']))
-            # Appends feature stem and category key
+        
+            # Append pos and oth for W_guess
+            if max(W_guess, key=W_guess.get) == ii['Answer']:
+                feat[ii['Answer']] = True
+                pos_train.append(feat)
+            else:
+                feat[ii['Answer']] = False
+                oth_train.append(feat)
 
-    	full_train.append((feat, ii['Answer']))
+            # Append pos and oth for Q_guess
+            if max(Q_guess, key=Q_guess.get) == ii['Answer']:
+                feat[ii['Answer']] = True
+                pos_train.append(feat)
+            else:
+                feat[ii['Answer']] = False
+                oth_train.append(feat)
 
-    print dev_train[0]
+        # if max(W_guess, key=W_guess.get) == ii['Answer']:
+        #     pos_full.append(W_guess)
+        # else:
+        #     oth_full.append(W_guess)
+
     # Train a classifier
     print("Training classifier ...")
-    classifier = nltk.classify.NaiveBayesClassifier.train(dev_train)
+    classifier = nltk.classify.PositiveNaiveBayesClassifier.train(pos_train, oth_train)
     # classifier = nltk.classify.MaxentClassifier.train(dev_train, 'IIS', trace=3, max_iter=5)
 
+    # print pos_test[6]
+    # test = pos_test[0]
+    # print classifier.classify(test)
 
     # Test the classfier 
-    right = 0
-    total = len(dev_test)
-    for ii in dev_test:
-        prediction = classifier.classify(ii[0])
-        if prediction == ii[1]:
-            right += 1
+    pos_right = 0
+    oth_right = 0
+    pos_total = len(pos_test)
+    oth_total = len(oth_test)
+
+    # Test for 'True' with positives
+    for ii in pos_test:
+        prediction = classifier.classify(ii)
+        # print "Answer: ", max(ii, key=ii.get)
+        if prediction == True:
+            pos_right += 1
+
+    # Test for 'False' with others
+    for ii in oth_test:
+        prediction = classifier.classify(ii)
+        # print "Answer: ", max(ii, key=ii.get)
+        if prediction == False:
+            oth_right += 1
+
+    right = pos_right + oth_right
+    total = pos_total + oth_total
     print("Accuracy on dev: %f" % (float(right) / float(total)))
 
-    # Retrain on all data
-    classifier = nltk.classify.NaiveBayesClassifier.train(dev_train + dev_test)
+    # # Retrain on all data
+    # print("Training classifier on all training data...")
+    # classifier = nltk.classify.PositiveNaiveBayesClassifier.train((pos_test + pos_train),(oth_test + oth_train))
     
-    # Read in test section
-    test = {}
-    for ii in DictReader(open("test.csv")):
-        test[ii['Question ID']] = classifier.classify(fe.text_features(ii['Question Text']))
+    # # Read in test section
+    # test = {}
+    # for ii in DictReader(open("test.csv")):
+    #     test[ii['Question ID']] = classifier.classify(fe.guess_dict(ii['IR_Wiki Scores']))
 
     # # Write predictions
     # o = DictWriter(open('pred.csv', 'w'), ['id', 'pred'])
